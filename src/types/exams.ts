b5ca2ec119b08d06ccd9d87b966c484e7c795887
gustavomuneyuki-afk <gustavo.muneyuki@gmail.com>
2026-09-08 11:50:44 -0300

    Add immunology and 1,25 vitamin D types

--- src/types/exams.ts
+++ src/types/exams.ts
@@ -1,13 +1,10 @@
 export type HemogramData = {
   hemoglobin?: string;
   hematocrit?: string;
-
   leukocytes?: number;
   neutrophils?: number;
   lymphocytes?: number;
-
   platelets?: number;
-
   reticulocytesAbsolute?: number;
   reticulocytesRelative?: string;
 };
@@ -18,82 +15,14 @@ export type GlycemicData = {
   insulin?: string;
 };
 
-export type RenalData = {
-  urea?: string;
-  creatinine?: string;
-  estimatedGfr?: string;
-};
-
-export type LipidData = {
-  totalCholesterol?: string;
-  hdl?: string;
-  ldl?: string;
-  vldl?: string;
-  triglycerides?: string;
-};
-
-export type ElectrolyteData = {
-  sodium?: string;
-  potassium?: string;
-  phosphorus?: string;
-  magnesium?: string;
-  ionizedCalcium?: string;
-  uricAcid?: string;
-};
-
-export type LiverData = {
-  ast?: string;
-  alt?: string;
-  ggt?: string;
-  alkalinePhosphatase?: string;
-  totalBilirubin?: string;
-  directBilirubin?: string;
-  indirectBilirubin?: string;
-  albumin?: string;
-  totalProtein?: string;
-};
-
-export type UrineProteinData = {
-  urineProtein?: string;
-  urineCreatinine?: string;
-  urineAlbumin?: string;
-  proteinCreatinineRatio?: string;
-  albuminCreatinineRatio?: string;
-};
-
-export type HormoneData = {
-  tsh?: string;
-  freeT4?: string;
-  acth?: string;
-  cortisol?: string;
-  pth?: string;
-};
-
-export type UrinalysisData = {
-  density?: string;
-  ph?: string;
-  protein?: string;
-  glucose?: string;
-  bilirubin?: string;
-  ketones?: string;
-  blood?: string;
-  nitrite?: string;
-  urobilinogen?: string;
-  epithelialCells?: string;
-  leukocytes?: string;
-  redBloodCells?: string;
-  crystals?: string;
-  casts?: string;
-  bacteria?: string;
-};
-
-export type InflammatoryData = {
-  crp?: string;
-  procalcitonin?: string;
-  esr?: string;
-  ferritin?: string;
-  haptoglobin?: string;
-};
+export type RenalData = { urea?: string; creatinine?: string; estimatedGfr?: string; };
+export type LipidData = { totalCholesterol?: string; hdl?: string; ldl?: string; vldl?: string; triglycerides?: string; };
+export type ElectrolyteData = { sodium?: string; potassium?: string; phosphorus?: string; magnesium?: string; ionizedCalcium?: string; uricAcid?: string; };
+export type LiverData = { ast?: string; alt?: string; ggt?: string; alkalinePhosphatase?: string; totalBilirubin?: string; directBilirubin?: string; indirectBilirubin?: string; albumin?: string; totalProtein?: string; };
+export type UrineProteinData = { urineProtein?: string; urineCreatinine?: string; urineAlbumin?: string; proteinCreatinineRatio?: string; albuminCreatinineRatio?: string; };
+export type HormoneData = { tsh?: string; freeT4?: string; acth?: string; cortisol?: string; pth?: string; };
+export type UrinalysisData = { density?: string; ph?: string; protein?: string; glucose?: string; bilirubin?: string; ketones?: string; blood?: string; nitrite?: string; urobilinogen?: string; epithelialCells?: string; leukocytes?: string; redBloodCells?: string; crystals?: string; casts?: string; bacteria?: string; };
+export type InflammatoryData = { crp?: string; procalcitonin?: string; esr?: string; ferritin?: string; haptoglobin?: string; };
 
 export type VitaminMetabolismData = {
   iron?: string;
@@ -102,30 +31,25 @@ export type VitaminMetabolismData = {
   vitaminB12?: string;
   folate?: string;
   vitaminD?: string;
+  vitaminD125?: string;
   zinc?: string;
 };
 
-export type CardiacMuscleData = {
-  cpk?: string;
-  ckmb?: string;
-  troponin?: string;
-  proBnp?: string;
+export type ImmunologyData = {
+  igG?: string;
+  igA?: string;
+  igM?: string;
+  igE?: string;
+  c3?: string;
+  c4?: string;
+  antiSsa?: string;
+  antiSsb?: string;
+  antiSm?: string;
 };
 
-export type CoagulationData = {
-  prothrombinTime?: string;
-  inr?: string;
-  aptt?: string;
-  fibrinogen?: string;
-  dDimer?: string;
-};
-
-export type TumorMarkerData = {
-  afp?: string;
-  psaTotal?: string;
-  psaFree?: string;
-  psaFreeTotalRatio?: string;
-};
+export type CardiacMuscleData = { cpk?: string; ckmb?: string; troponin?: string; proBnp?: string; };
+export type CoagulationData = { prothrombinTime?: string; inr?: string; aptt?: string; fibrinogen?: string; dDimer?: string; };
+export type TumorMarkerData = { afp?: string; psaTotal?: string; psaFree?: string; psaFreeTotalRatio?: string; };
 
 export type ExamResult = {
   hemogram: HemogramData;
@@ -137,6 +61,7 @@ export type ExamResult = {
   inflammatory: InflammatoryData;
   hormones: HormoneData;
   vitaminMetabolism: VitaminMetabolismData;
+  immunology: ImmunologyData;
   cardiacMuscle: CardiacMuscleData;
   coagulation: CoagulationData;
   urineProtein: UrineProteinData;
