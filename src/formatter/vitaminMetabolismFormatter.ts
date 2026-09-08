bf0245bbacf131ca8cb5f4eadcec13d553c29054
gustavomuneyuki-afk <gustavo.muneyuki@gmail.com>
2026-09-08 11:55:07 -0300

    Adjust vitamin D output nomenclature

--- src/formatter/vitaminMetabolismFormatter.ts
+++ src/formatter/vitaminMetabolismFormatter.ts
@@ -7,8 +7,8 @@ export function formatVitaminMetabolism(data: VitaminMetabolismData): string {
   if (data.transferrinSaturation) parts.push(`Sat Transf ${data.transferrinSaturation}%`);
   if (data.vitaminB12) parts.push(`B12 ${data.vitaminB12}`);
   if (data.folate) parts.push(`Fol ${data.folate}`);
-  if (data.vitaminD) parts.push(`Vit D ${data.vitaminD}`);
-  if (data.vitaminD125) parts.push(`Vit D 1,25 ${data.vitaminD125}`);
+  if (data.vitaminD) parts.push(`25-Vit D ${data.vitaminD}`);
+  if (data.vitaminD125) parts.push(`1,25-Vit D ${data.vitaminD125}`);
   if (data.zinc) parts.push(`Zn ${data.zinc}`);
   return parts.join(" / ");
 }
