02fc84c8b32ce883afc39f89f531d4eb8222ceaf
gustavomuneyuki-afk <gustavo.muneyuki@gmail.com>
2026-09-08 11:51:21 -0300

    Integrate immunology parser

--- src/parser/parseAllExams.ts
+++ src/parser/parseAllExams.ts
@@ -1,107 +1,37 @@
 import type { ExamResult } from "../types/exams";
 import type { PdfLine } from "../utils/pdfReader";
+import { parseCardiacMuscle } from "./cardiacMuscleParser";
+import { parseCoagulation } from "./coagulationParser";
+import { parseElectrolytes } from "./electrolyteParser";
+import { parseGlycemic } from "./glycemicParser";
+import { parseHemogram } from "./hemogramParser";
+import { parseHormones } from "./hormoneParser";
+import { parseInflammatory } from "./inflammatoryParser";
+import { parseImmunology } from "./immunologyParser";
+import { parseLipidProfile } from "./lipidProfileParser";
+import { parseLiver } from "./liverParser";
+import { parseRenal } from "./renalParser";
+import { parseVitaminMetabolism } from "./vitaminMetabolismParser";
+import { parseUrinalysis } from "./urinalysisParser";
+import { parseUrineProtein } from "./urineProteinParser";
+import { parseTumorMarkers } from "./tumorMarkerParser";
 
-import {
-  parseCardiacMuscle,
-} from "./cardiacMuscleParser";
-
-import {
-  parseCoagulation,
-} from "./coagulationParser";
-
-import {
-  parseElectrolytes,
-} from "./electrolyteParser";
-
-import {
-  parseGlycemic,
-} from "./glycemicParser";
-
-import {
-  parseHemogram,
-} from "./hemogramParser";
-
-import {
-  parseHormones,
-} from "./hormoneParser";
-
-import {
-  parseInflammatory,
-} from "./inflammatoryParser";
-
-import {
-  parseLipidProfile,
-} from "./lipidProfileParser";
-
-import {
-  parseLiver,
-} from "./liverParser";
-
-import {
-  parseRenal,
-} from "./renalParser";
-
-import {
-  parseVitaminMetabolism,
-} from "./vitaminMetabolismParser";
-
-import {
-  parseUrinalysis,
-} from "./urinalysisParser";
-
-import {
-  parseUrineProtein,
-} from "./urineProteinParser";
-
-import {
-  parseTumorMarkers,
-} from "./tumorMarkerParser";
-
-export function parseAllExams(
-  lines: PdfLine[],
-): ExamResult {
+export function parseAllExams(lines: PdfLine[]): ExamResult {
   return {
-    hemogram:
-      parseHemogram(lines),
-
-    glycemic:
-      parseGlycemic(lines),
-
-    renal:
-      parseRenal(lines),
-
-    lipid:
-      parseLipidProfile(lines),
-
-    electrolytes:
-      parseElectrolytes(lines),
-
-    liver:
-      parseLiver(lines),
-
-    inflammatory:
-      parseInflammatory(lines),
-
-    hormones:
-      parseHormones(lines),
-
-    vitaminMetabolism:
-      parseVitaminMetabolism(lines),
-
-    cardiacMuscle:
-      parseCardiacMuscle(lines),
-
-    coagulation:
-      parseCoagulation(lines),
-
-    urinalysis:
-     parseUrinalysis(lines),
-
-     urineProtein:
-  parseUrineProtein(lines),
-
-     tumorMarkers:
-  parseTumorMarkers(lines),
-      
+    hemogram: parseHemogram(lines),
+    glycemic: parseGlycemic(lines),
+    renal: parseRenal(lines),
+    lipid: parseLipidProfile(lines),
+    electrolytes: parseElectrolytes(lines),
+    liver: parseLiver(lines),
+    inflammatory: parseInflammatory(lines),
+    hormones: parseHormones(lines),
+    vitaminMetabolism: parseVitaminMetabolism(lines),
+    immunology: parseImmunology(lines),
+    cardiacMuscle: parseCardiacMuscle(lines),
+    coagulation: parseCoagulation(lines),
+    urinalysis: parseUrinalysis(lines),
+    urineProtein: parseUrineProtein(lines),
+    tumorMarkers: parseTumorMarkers(lines),
   };
 }
